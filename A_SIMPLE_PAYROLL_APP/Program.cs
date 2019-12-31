d65df24a20181df30ef5a604573005c4358ba260
Jeremy L. Shepherd <jeremylshepherd@gmail.com>
2019-12-31 13:27:21 -0500

    First Commit

---
+++ A_SIMPLE_PAYROLL_APP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A_SIMPLE_PAYROLL_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
