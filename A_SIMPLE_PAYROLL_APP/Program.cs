13ebf6f88a24516b3d257b015df7dc514cf50740
Jeremy L. Shepherd <jeremylshepherd@gmail.com>
2019-12-31 13:42:50 -0500

    Implementing Staff Class

--- A_SIMPLE_PAYROLL_APP/Program.cs
+++ A_SIMPLE_PAYROLL_APP/Program.cs
@@ -8,5 +8,34 @@ namespace A_SIMPLE_PAYROLL_APP
         {
             Console.WriteLine("Hello World!");
         }
+
+        class Staff
+        {
+            private double hourlyRate;
+            private int hWorked;
+
+            public double TotalPay { get; set; }
+            public double BasicPay { get; set; }
+            public string NameOfStaff { get; set; }
+
+            public Staff(string name, double rate)
+            {
+                NameOfStaff = name;
+                hourlyRate = rate;
+            }
+
+            public virtual void CalculatePay()
+            {
+                Console.WriteLine("Calculating Pay ...");
+                BasicPay = hWorked * hourlyRate;
+                TotalPay = BasicPay;
+            }
+
+            public override string ToString()
+            {
+                return $"Staff Name: {NameOfStaff}\n____________________________\nHourly Rate: {hourlyRate:C}\nHours Worked: {hWorked}\nBasic Pay: {BasicPay}\nTotal Pay: {TotalPay}";
+            }
+        }
+
     }
 }
